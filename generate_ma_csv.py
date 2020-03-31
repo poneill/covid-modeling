f713086b66c558979a8c79546a40460cb4553005
Patrick O'Neill <patrick.oneill@kensho.com>
2020-03-30 19:09:19 -0500

    commit generate_ma_csv

--- generate_ma_csv.py
+++ generate_ma_csv.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 
 from eda import ma_data
-from sir_fitting_us import seir_experiment, make_csv_from_traj
+from sir_fitting_us import seir_experiment, make_csv_from_ma_traj
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -12,13 +12,13 @@ logger.setLevel(logging.INFO)
 logger.info("Fitting model.")
 
 # initial values taken from previous fit, used to seed MH sampler efficiently.
-x0 = np.array([-0.19, -2.65, -3.21, -6.12, -19.60])
-traj = seir_experiment(ma_data, x0, iterations=100)
+x0 = np.array([  0.393,  -2.586,  -3.241,  -5.874, -24.999])
+ma_traj = seir_experiment(ma_data, x0, iterations=10000)
 
-mean_ll = np.mean([ll for (x, ll) in traj])
+mean_ll = np.mean([ll for (x, ll) in ma_traj])
 logger.info("Model fitting finished with mean log-likelihood: {}".format(mean_ll))
 
-if mean_ll < -20:
+if mean_ll < -2000:
     raise AssertionError(
         """Mean log-likelihood {} less than threshold of
         -20.  This is probably an error.""".format(mean_ll)
@@ -26,4 +26,4 @@ if mean_ll < -20:
 
 underscored_time = time.ctime().replace(" ", "_")
 fname = "ma_seir_output_{}.csv".format(underscored_time)
-make_csv_from_traj(traj, ma_data, fname)
+make_csv_from_ma_traj(ma_traj, ma_data, fname)
